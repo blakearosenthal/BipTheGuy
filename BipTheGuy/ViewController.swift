ed09ad5bfd61fa2f67bf3534732e048998f5f052
Blake Rosenthal <brosenthal@zagmail.gonzaga.edu>
2021-09-23 20:45:24 -0700

    Initial Commit

---
+++ BipTheGuy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BipTheGuy
+//
+//  Created by Rosenthal, Blake Alexander on 9/23/21.
+//  Copyright © 2021 Rosenthal, Blake Alexander. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
